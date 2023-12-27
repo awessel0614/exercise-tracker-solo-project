811fd26496e2643faab0bd48586fd40c7066704c
Ashley Wessel <amerino.dance@gmail.com>
2023-12-27 14:21:46 -0600

    added info to the database.sql file

--- database.sql
+++ database.sql
@@ -8,3 +8,34 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "session" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT references "user",
+	"session_date" DATE
+
+);
+
+CREATE TABLE "session_details" (
+	"id" SERIAL PRIMARY KEY,
+	"session_id" INT references "session",
+	"exercise_id" INT references "exercise",
+	"set_number" INT,
+	"reps" INT,
+	"weight" INT,
+	"isToggled" BOOLEAN DEFAULT FALSE
+);
+
+
+CREATE TABLE "exercise" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT references "user",
+	"exercise_name" VARCHAR (1000) NOT NULL
+);
+
+
+
+INSERT INTO "exercise" 
+	("exercise_name")
+	VALUES
+	('Squat'), ('Sumo Squat'), ('Deadlift'), ('Romanian Deadlift'), ('Bicep Curls');
