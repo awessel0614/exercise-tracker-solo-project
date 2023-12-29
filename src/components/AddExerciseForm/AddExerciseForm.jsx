cd8dfe21b5b23bd565b2b6cde35058d2bbc3c30f
Ashley Wessel <amerino.dance@gmail.com>
2023-12-29 00:04:26 -0600

    dropdown created, is populating the exercises from the database

--- src/components/AddExerciseForm/AddExerciseForm.jsx
+++ src/components/AddExerciseForm/AddExerciseForm.jsx
@@ -1,3 +1,67 @@
+// import axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { useHistory } from 'react-router-dom';
+// import { TextField, Button, Grid, Container } from "@mui/material";
+// import './AddExerciseForm.css';
+
+
+// function AddExerciseForm() {
+
+//     const dispatch = useDispatch();
+//     const exercises = useSelector(store => store.exercises)
+
+
+//     const getExercises = () => {
+//         axios.get('/api/exercise').then((response) => {
+//             const action = {type: 'SET_EXERCISES', payload: response.data};
+//             dispatch(action);
+//         }).catch((error) => {
+//             console.log('Error getting exercise list.', error);
+//             alert('Something went wrong!')
+//         })
+//     }
+
+//     useEffect(() => {
+//         getExercises();
+//     }, []);
+
+
+//     return (
+//         <>
+//         <h1>Enter Exercise Info:</h1>
+
+//         <h2>For test purposes, here's a list of exercises in the database:</h2>
+//         <ul>
+//             {exercises.map((exercise, i) => 
+//                 <li key={i}>
+//                     {exercise.exercise_name}
+//                 </li>
+//                 )}
+//         </ul>
+//         </>
+//     )
+
+// }
+
+// export default AddExerciseForm;
+
+
+// ^^ this is working as of 12/28 11 pm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
@@ -5,8 +69,27 @@ import { useHistory } from 'react-router-dom';
 import { TextField, Button, Grid, Container } from "@mui/material";
 import './AddExerciseForm.css';
 
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 
 function AddExerciseForm() {
+
+
+        
+        const [selectedExercise, setSelectedExercise] = React.useState('');
+      
+        const handleChange = (event) => {
+          setSelectedExercise(event.target.value);
+        };
+
+
+
+
+
 
     const dispatch = useDispatch();
     const exercises = useSelector(store => store.exercises)
@@ -15,6 +98,7 @@ function AddExerciseForm() {
     const getExercises = () => {
         axios.get('/api/exercise').then((response) => {
             const action = {type: 'SET_EXERCISES', payload: response.data};
+            console.log("this is the payload", response.data )
             dispatch(action);
         }).catch((error) => {
             console.log('Error getting exercise list.', error);
@@ -39,6 +123,30 @@ function AddExerciseForm() {
                 </li>
                 )}
         </ul>
+
+    
+        <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Select Exercise</InputLabel>
+                <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedExercise}
+                label="selectedExercise"
+                name="selectedExercise"
+                onChange={handleChange}
+                >
+                {exercises.map((exercise, i) =>
+                    <MenuItem key={i}>{exercise.exercise_name}</MenuItem>
+                )}    
+                </Select>
+            </FormControl>
+        </Box>
+
+
+
+
+
         </>
     )
 
