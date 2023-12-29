5e42f28c42d0140dd71c39f10d274b6f3f86e58b
Ashley Wessel <amerino.dance@gmail.com>
2023-12-29 13:15:13 -0600

    saving just in case

--- src/components/AddExerciseForm/AddExerciseForm.jsx
+++ src/components/AddExerciseForm/AddExerciseForm.jsx
@@ -14,25 +14,40 @@ import Select from '@mui/material/Select';
 
 function AddExerciseForm() {
 
-
+        const [selectedExerciseName, setSelectedExerciseName] = useState ('testing')
         
         const [selectedExercise, setSelectedExercise] = useState({
             //session_id: '',
             exercise_id: 'test',
             set_number: '',
             reps: '',
-            weight: '',
+            //weight: '',
             //isToggled: false
         });
       
-        const handleExerciseChange = (event) => {
-            
+        const handleExerciseChange = (event) => { 
            setSelectedExercise({
                 ...selectedExercise,
                 exercise_id: event.target.value,
            }) 
-           console.log("this is the event.target.value", event.target.value);
+           console.log("this is the event.target.value for exercise ID", event.target.value);
         };
+
+        const handleSetChange = (event) => { 
+            setSelectedExercise({
+                 ...selectedExercise,
+                 set_number: event.target.value,
+            }) 
+            console.log("this is the event.target.value for set number", event.target.value);
+         };
+
+         const handleRepChange = (event) => { 
+            setSelectedExercise({
+                 ...selectedExercise,
+                 reps: event.target.value,
+            }) 
+            console.log("this is the event.target.value for rep number", event.target.value);
+         };
 
 
 
@@ -70,18 +85,67 @@ function AddExerciseForm() {
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
                     label="selectedExercise"
-                    name="selectedExercise"
+                    //name="selectedExercise.exercise_id"
                     onChange={handleExerciseChange}
                     >
                     {exercises.map((exercise, i) =>
-                        <MenuItem key={i} value={exercise.id}>{exercise.exercise_name}</MenuItem>
+                        <MenuItem key={i} name={exercise.exercise_name} value={exercise.id}>{exercise.exercise_name}</MenuItem>
                     )}    
                     </Select>
+                    <br></br>
+                <Grid alignItems="center">
+                    <TextField 
+                    id="outlined-basic" 
+                    label="Set1" 
+                    variant="outlined" 
+                    style={{maxWidth: '50px', maxHeight: '30px', minWidth: '60px', minHeight: '30px'}}
+                    onChange={handleSetChange}
+                    />
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <TextField 
+                    id="outlined-basic" 
+                    label="Set2" 
+                    variant="outlined" 
+                    style={{maxWidth: '50px', maxHeight: '30px', minWidth: '60px', minHeight: '30px'}}
+                    onChange={handleSetChange}
+                    />
+                
+                
+                    <Button 
+                    variant="contained"
+                    style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}
+                    >+</Button>
+                    <Button 
+                    variant="contained"
+                    style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}
+                    >-</Button>
+                </Grid>
+                <br></br>
+                <br></br>
+                <TextField 
+                    id="outlined-basic" 
+                    label="Rep1" 
+                    variant="outlined" 
+                    style={{maxWidth: '50px', maxHeight: '30px', minWidth: '80px', minHeight: '30px'}}
+                    onChange={handleRepChange}
+                    />
+                <br></br>
+                <br></br>
+                <TextField 
+                    id="outlined-basic" 
+                    label="Rep2" 
+                    variant="outlined" 
+                    style={{maxWidth: '50px', maxHeight: '30px', minWidth: '80px', minHeight: '30px'}}
+                    onChange={handleRepChange}
+                    />
+
             </FormControl>
         </Box>
 
+        <br></br>
         <p>this is the id of the selected exercise: {selectedExercise.exercise_id}</p>
-
         </>
     )
 
