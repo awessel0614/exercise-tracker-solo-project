5d1c10439520d9d19354324a741354df0f6b5f43
Ashley Wessel <amerino.dance@gmail.com>
2024-01-20 11:22:12 -0600

    added styling to the home page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,3 +1,72 @@
+// import React, { useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+// import './LandingPage.css';
+
+// // CUSTOM COMPONENTS
+// import RegisterForm from '../RegisterForm/RegisterForm';
+
+// function LandingPage() {
+//   const [heading, setHeading] = useState('Welcome');
+//   const history = useHistory();
+
+//   const onLogin = (event) => {
+//     history.push('/login');
+//   };
+
+//   return (
+//     <div className="container">
+//       <h2>{heading}</h2>
+
+//       <div className="grid">
+//         <div className="grid-col grid-col_8">
+//           <p>
+//             Welcome to your Exercise Tracker App!
+//           </p>
+
+//         </div>
+//         <div className="grid-col grid-col_4">
+//           <RegisterForm />
+
+//           <center>
+//             <h4>Already a Member?</h4>
+//             <button className="btn btn_sizeSm" onClick={onLogin}>
+//               Login
+//             </button>
+//           </center>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default LandingPage;
+
+// ^^ what was there originally
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
@@ -15,22 +84,25 @@ function LandingPage() {
 
   return (
     <div className="container">
-      <h2>{heading}</h2>
+      <h1 id="meet">Meet your Exercise Calendar!</h1>
 
       <div className="grid">
         <div className="grid-col grid-col_8">
-          <p>
-            Welcome to your Exercise Tracker App!
+          <p id="paragraph">
+            Leave your notebook at home--
+            here's an easy way to keep track of your exercises, so that you don't
+            have to sweat the small stuff!
           </p>
 
         </div>
         <div className="grid-col grid-col_4">
-          <RegisterForm />
-
+          {/* <RegisterForm sx ={{float: "left"}}/> */}
+         {/* <br></br>  */}
+         <br></br> 
           <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
+            {/* <h2 >Let's Go!</h2> */}
+            <button className="btn btn_sizeM" onClick={onLogin}>
+              Register / Login
             </button>
           </center>
         </div>
